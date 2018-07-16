88947eae403019acb72ec5189d0e44cd744337ab
Aaron Mednick <aaronjosephm@gmail.com>
2018-07-16 16:14:43 -0400

    hacker proof

--- recipe.rb
+++ recipe.rb
@@ -4,10 +4,19 @@ class Recipe
   attr_accessor :name, :description, :prep_time, :done, :difficulty
 
   def initialize(name, description, prep_time, done = " ", difficulty = "")
-    @name = name
-    @description = description
-    @prep_time = prep_time
-    @done = done
-    @difficulty = difficulty
+    @name = html_safe(name)
+    @description = html_safe(description)
+    @prep_time = html_safe(prep_time)
+    @done = html_safe(done)
+    @difficulty = html_safe(difficulty)
+  end
+
+  private
+  def html_safe(par)
+    if par.nil?
+      return par
+    else
+      return par.gsub("<", "&lt;").gsub(">", "&gt;")
+    end
   end
 end
